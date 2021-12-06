8737fcfaebcbbf7ea441ffea534ea2841adc3e8f
aw2884463 <awroblos1@student.rccd.edu>
2021-12-05 18:29:30 -0800

    Initial Card Class

---
+++ Versions/v1/main.cpp
@@ -0,0 +1,38 @@
+/*
+ * Author:Austin Wroblos
+ * Course:CIS-17A
+ * Date:12/3/2021
+ * Time:2:04PM
+ */
+
+
+#include "PlayingCard.h"    // Playing card class
+
+#include <cstdlib>          // Standard library
+#include <ctime>            // Time Library
+#include <iostream>         // Input Output
+#include <iomanip>          // Input/Output manipulation
+#include <string>           // Getline
+#include <fstream>          // Input output file stream
+#include <array>            // Array library
+#include <vector>           // Vector Library
+#include <cctype>           // Upper and Lowercase manipulation
+
+using namespace std;
+
+
+void startGame();
+
+int main() {
+    
+    
+
+    return 0;
+}
+
+
+void startGame() {
+    
+
+}
+
